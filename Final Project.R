c32a3962e9f71170a6cb77dc43233d349818f434
Dennis-Huang2995 <62626680+Dennis-Huang2995@users.noreply.github.com>
2020-04-08 22:59:40 -0400

    Update Final Project.R

--- Final Project.R
+++ Final Project.R
@@ -15,9 +15,15 @@ library(forecast)
 library(depmixS4)
 fredr_set_key('30e6ecb242a73869e11cb35f6aa3afc3') # Copy and paste your FREDR key.
 
+getwd() # Identify the working directory.
+setwd("/Schack Spring Real Estate Data Science")
 
 
-VIX = drop_na(fredr("VIXCLS", observation_start = as.Date("1982-01-01")))
+VIX = read.csv('VIXCLS.csv')
+names = c("date", "VIX", "yieldcurve", "recession")
+colnames(VIX) = names
+
+
 recession = fredr('USREC')
 yieldcurve = fredr('T10Y3MM')
 
@@ -25,24 +31,17 @@ data = merge(recession, yieldcurve, by.x='date', by.y='date')  ## Merge into sin
 data = subset(data, select = c(date, value.x, value.y))
 names = c("date", "recession", "yieldcurve")
 colnames(data) = names
-data1 = merge(data, VIX, by.x='date', by.y='date')
-data1 = subset(data1, select = c(date,recession, yieldcurve, value))
-names = c("date", "recession", "yieldcurve","VIX")
-colnames(data1) = names
 
-data1$yieldcurve = data1$yieldcurve * 100
-summary(data1)
 
-data1$yieldcurve.l1 = lag(data1$yieldcurve, 1)
+VIX$yieldcurve = VIX$yieldcurve * 100
+summary(VIX)
 
-plot(data1$date, data1$yieldcurve, col="darkblue", main="U.S. Yield Curve (10-Year Minus 3-Month)", pch=16, 
-     xlab="Date", ylab="Basis Points", ylim=c(-100, 450))
-lines(data1$date, data1$yieldcurve, col="darkblue")
-grid(lw=2)
-abline(h=0, col="red")
-abline(h=mean(data1$yieldcurve), col='darkgreen')
+VIX$yieldcurve.l1 = lag(VIX$yieldcurve, 1)
 
-camharvey = lm(recession ~ yieldcurve + yieldcurve.l1 + VIX, data=data1)
+camharvey = lm(recession ~ yieldcurve + yieldcurve.l1 + VIX, data=VIX)
 stargazer(camharvey, type="text", title="Recession Predictor", single.row=TRUE, 
           ci=TRUE, ci.level=0.95)
 
+camharvey = lm(recession ~ yieldcurve + yieldcurve.l1, data=VIX)
+stargazer(camharvey, type="text", title="Recession Predictor", single.row=TRUE, 
+          ci=TRUE, ci.level=0.95)
